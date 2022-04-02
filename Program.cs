b3913a965f16486945fd3e4d09d7f8d0ea11e155
Macarena061199 <102336411+Macarena061199@users.noreply.github.com>
2022-04-02 18:06:09 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+bool band = false;
+Console.Write("Por favor ingrese su nombre: ");
+string nombre = Console.ReadLine();
+do
+{
+   
+    Console.WriteLine("Hola " + nombre + ".");
+
+    Console.Write("Por favor " + nombre + " ingresa una frase: ");
+    string frase = Console.ReadLine();
+    string fin = "fin";
+    bool flag = frase.Contains(fin);
+
+    if (flag == true)
+    {
+        Console.WriteLine("La frase ingresada contiene la palabra " + fin + ".");
+        band = true;
+    }
+} while (band == false);
